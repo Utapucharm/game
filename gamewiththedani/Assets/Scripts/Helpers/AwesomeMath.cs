881ba942200172f4985712695ec9b4c89ccfd477
Daniel <logitin@pm.me>
2022-11-28 20:25:42 +0100

    Removed helpers as lesson prep

--- gamewiththedani/Assets/Scripts/Helpers/AwesomeMath.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class AwesomeMath
-{
-    public static Vector2 CalculateCircleCoordinate(float radius, float angle)
-    {
-        return new Vector2(radius * Mathf.Cos(angle * Mathf.Deg2Rad), radius * Mathf.Sin(angle * Mathf.Deg2Rad));
-    }
-}
