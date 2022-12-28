afb91424d4cc2b313cbf79cd34da1e563ee511ab
Utapucharm <matti.schlup@gmx.net>
2022-12-28 21:14:25 +0100

    buggy spread

--- gamewiththedani/Assets/Scripts/Weapons & Interactions/Shotgun.cs
+++ gamewiththedani/Assets/Scripts/Weapons & Interactions/Shotgun.cs
@@ -5,25 +5,23 @@ public class Shotgun : MonoBehaviour, IFirable
     [SerializeField] private GameObject projectile;
     [SerializeField] private Transform shootPoint;
     [SerializeField] private Vector3 shootForce;
+    [SerializeField] private int popcornNumber = 5;
+    [SerializeField] private float spreadPopcorn = 6.9f;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        { 
-            Fire();
-        }
-    }
 
     public void Fire()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < popcornNumber; i++)
         {
-            Vector3 offset = CoolMath.CalculateCirclePoint(0.69f, 360/5*i); 
+            Vector3 offset = CoolMath.CalculateCirclePoint(0.69f, 360/popcornNumber*i); 
             GameObject currentProjectile = Instantiate(projectile, shootPoint.position + transform.TransformDirection(offset), shootPoint.rotation);
             Rigidbody burgerRigidbody = currentProjectile.GetComponent<Rigidbody>();
             burgerRigidbody.mass = 1000;
-       
-            burgerRigidbody.AddForce(currentProjectile.transform.TransformDirection(shootForce) * burgerRigidbody.mass);
+            
+            Vector3 direction = offset - shootPoint.position;
+            direction = direction.normalized;
+            
+            burgerRigidbody.AddForce((shootPoint.transform.TransformDirection(shootForce)+ direction * spreadPopcorn) * burgerRigidbody.mass);
         }
     }
 }
